092b7bbb3cdf6c87bbff02c79c36c104e22115f9
claraneves23 <mariaclarapn14@gmail.com>
2025-02-26 19:30:08 -0300

    python (fix): "mudança das variaveis de protected para privado"

--- comparacao_python/get_set/Dados_Contribuinte.py
+++ comparacao_python/get_set/Dados_Contribuinte.py
@@ -1,41 +1,41 @@
 class Dados_Contribuinte:
     def __init__(self, nome="", cpf="", cnpj="", cartao=""):
-        self._nome = nome
-        self._cpf = cpf
-        self._cnpj = cnpj
-        self._cartao = cartao
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__cnpj = cnpj
+        self.__cartao = cartao
 
     @property
     def nome(self):
-        return self._nome
+        return self.__nome
     
     @nome.setter
     def set_nome(self, nome): 
-        self._nome = nome
+        self.__nome = nome
 
     @property
     def cpf(self):
-        return self._cpf
+        return self.__cpf
     
     @cpf.setter
     def set_cpf(self, cpf): 
-        self._cpf = cpf
+        self.__cpf = cpf
 
     @property
     def cnpj(self):
-        return self._cpf
+        return self.__cnpj
 
     @cnpj.setter
     def set_cnpj(self, cnpj): 
-        self._cnpj = cnpj
+        self.__cnpj = cnpj
 
     @property
     def cartao(self):
-        return self._cartao
+        return self.__cartao
 
     @cartao.setter
     def set_cartao(self, cartao): 
-        self._cartao = cartao
+        self.__cartao = cartao
 
    
     
--- comparacao_python/get_set/Novo_Automovel.py
+++ comparacao_python/get_set/Novo_Automovel.py
@@ -1,38 +1,38 @@
 class novo_automovel:
     def __init__(self,marca="", modelo="", cor="",preco=0):
-        self._marca = marca
-        self._modelo = modelo
-        self._cor = cor 
-        self._preco = preco
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__cor = cor 
+        self.__preco = preco
 
     @property
     def marca(self):
-        return self._marca
+        return self.__marca
     
     @marca.setter
     def set_marca(self, marca): 
-        self._marca = marca
+        self.__marca = marca
 
     @property
     def modelo(self):
-        return self._modelo
+        return self.__modelo
     
     @modelo.setter
     def set_modelo(self, modelo): 
-        self._modelo = modelo
+        self.__modelo = modelo
     
     @property
     def cor(self):
-        return self._cor
+        return self.__cor
     
     @modelo.setter
     def set_cor(self, cor): 
-        self._cor = cor
+        self.__cor = cor
 
     @property
     def preco(self):
-        return self._preco
+        return self.__preco
     
     @preco.setter
     def set_preco(self, preco): 
-        self._preco = preco
+        self.__preco = preco
