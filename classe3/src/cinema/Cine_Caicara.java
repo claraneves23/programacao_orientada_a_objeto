465e625fdf13a6834f9488a3f43a606e9e1ca067
claraneves23 <mariaclarapn14@gmail.com>
2025-04-14 10:54:18 -0300

    passando parametros no super

---
+++ classe3/src/cinema/Cine_Caicara.java
@@ -0,0 +1,8 @@
+package cinema;
+
+public class Cine_Caicara extends Cinemas_Antigos {
+
+	Cine_Caicara(String antigo){
+		super(antigo);
+	}
+}
---
+++ classe3/src/cinema/Cinemas_de_Santos.java
@@ -0,0 +1,13 @@
+package cinema;
+
+public class Cinemas_de_Santos {
+	private String localizacao;
+	
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	
+	public String getLocalizacao() {
+		return localizacao;
+	}
+}
